96bb7af2603d16ccf8a2a243336aca19774024bd
Pokiza Sdv <pokizasdv@gmail.com>
2023-11-17 23:48:50 -0500

    Solve Problem checking for Palindrome

--- index3.js
+++ index3.js
@@ -133,3 +133,20 @@ const isAnagram = (str1, str2) => {
 
 console.log("___________________________");
 console.log(isAnagram("secure", "rescue"));
+
+
+// Create a function that checks if a given string is a palindrome (the same forwards and backwards).
+// Input: hello => output: false
+// Input: rotetor => output: true
+
+const isPalindrome = (str) => {
+    for (let i = 0; i < str.length / 2; i++) {
+      if (str[i] !== str[str.length - i - 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  console.log(isPalindrome("rotetor")); 
+  console.log(isPalindrome("hello"));
