80a223b138d222edae3bdc3d8565cf3c62f59fa8
Pokiza Sdv <pokizasdv@gmail.com>
2023-11-17 23:39:43 -0500

    Solve Problem Summing up natural number array

--- index3.js
+++ index3.js
@@ -17,3 +17,17 @@ const convertToObjArr = (strings) => {
 };
 console.log("_____________________________");
 console.log(convertToObjArr([1, 2, 3, 4]));
+
+// [1,2,3,4,5,6,7,8,9,10]
+// Sum up array. but do not use loop
+// 1, 2, 3 = 6; 3 * 4 / 2 = 6
+// 1, 2, 3, 4 = 10; 4 * 5 / 2 = 10
+// 1, 2, 3, 4, 5 = 15; 5 * 6  / 2 = 15;
+
+// (n * (n +1) )/2
+
+const sumUpArr = (arr) => {
+    return (arr.length * (arr.length + 1)) / 2;
+};
+console.log("______________________________");
+console.log(sumUpArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
